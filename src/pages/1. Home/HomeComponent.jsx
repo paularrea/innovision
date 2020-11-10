ab33ef925303ee679b12ce7a6550932c91440560
Pau Larrea <paularrea12@hotmail.com>
2020-11-10 15:11:52 +0100

    react-helmet

--- src/pages/1. Home/HomeComponent.jsx
+++ src/pages/1. Home/HomeComponent.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./home.css";
+import { Helmet } from "react-helmet";
 import FadeInOnScroll from "../../components/FadeInOnScroll";
 
 function HomeComponent(props) {
   return (
     <div ref={props.HomeRef} className="page">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Innovision Medical</title>
+        <meta name="description" content="Helmet app" />
+      </Helmet>
       <div className="landing-start">
         <div className="landing-start-content">
           <h2>{props.t("home.title")}</h2>
