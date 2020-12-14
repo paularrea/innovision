eec35cf55edf79fbf93259fe535972bccc865d8e
Pau Larrea <paularrea12@hotmail.com>
2020-12-14 15:42:04 +0100

    lazylanding

--- src/pages/1. Home/components/LandingImg.jsx
+++ src/pages/1. Home/components/LandingImg.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import img from "../../../img/home.jpg";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 const LandingImg = () => {
   return (
     <div>
-      <img src={img} alt="landing" />
+      <LazyLoadImage effect="blur" src={img} alt="landing" />
     </div>
   );
 };
