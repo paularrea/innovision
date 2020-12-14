28ab2a005cdddde43189821e26ad838b8f9908f4
Pau Larrea <paularrea12@hotmail.com>
2020-12-14 16:02:30 +0100

    video resized

--- src/pages/1. Home/components/LandingImg.jsx
+++ src/pages/1. Home/components/LandingImg.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import img from "../../../img/home.jpg";
-import { LazyLoadImage } from "react-lazy-load-image-component";
-import "react-lazy-load-image-component/src/effects/blur.css";
+// import { LazyLoadImage } from "react-lazy-load-image-component";
+// import "react-lazy-load-image-component/src/effects/blur.css";
 
 const LandingImg = () => {
   return (
     <div>
-      <LazyLoadImage effect="blur" src={img} alt="landing" />
+      {/* <LazyLoadImage effect="blur" className='landing-img' src={img} alt="landing" /> */}
+      <img src={img} className='landing-img' alt="landing"/>
     </div>
   );
 };
