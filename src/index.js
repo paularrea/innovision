78984a057b13bb7f973a9d2e43218ed75822c80c
Pau Larrea <paularrea12@hotmail.com>
2020-12-15 15:55:39 +0100

    animation intro

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
-import App from "./App";
+// import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 import "./i18n";
-import logo from "../src/img/logo.png";
+import logo from '../src/img/logo.png'
+
+const App = React.lazy(() => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(import("./App")), 2500);
+  });
+});
 
 ReactDOM.render(
   <Router>
@@ -13,7 +19,7 @@ ReactDOM.render(
       <Suspense
         fallback={
           <div className="intro-logo-container">
-            <div className="intro-logo">
+            <div className="intro-logo animate apear">
               <img src={logo} alt="innovision logo" />
             </div>
           </div>
